c05c7a742e6125a1d5244959dcdc30874497cd78
MarkBrianK <kariukimarkbrian@gmail.com>
2022-10-14 11:53:25 +0300

    Deploying backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
 
-  # has_secure_password
-  # has_one :farmer
+  has_secure_password
+  has_one :farmer
 
 
 
-  # validates :email, presence: true, uniqueness: true
-  # validates :password, length: {minimum:4}
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: {minimum:4}
 end
