1c58695e6955030e35184ef1ed8012789e48bbd6
MarkBrianK <kariukimarkbrian>
2022-10-16 23:43:31 +0300

    adding session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email], password_digest: params[:password])
     session[:user_id] = user[:id]
-    render json:user
+    render json: user
   end
 
   # logging out
