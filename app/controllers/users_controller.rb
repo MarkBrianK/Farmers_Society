c6f637a13f7656de6b5cefd71c5ab605d6242b76
MarkBrianK <kariukimarkbrian>
2022-10-16 22:14:11 +0300

    digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.permit( :email, :password,  :password_confirmation)
+    params.permit( :email, :farmer_id, :password_digest,  :password_confirmation)
 
   end
 
