334a07a4323126c02deaa5fda7972c2f0bc950e6
MarkBrianK <kariukimarkbrian>
2022-10-16 22:19:45 +0300

    digest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
       user = User.find_by(email: params[:email])
 
-      if user && user.authenticate(params[:password])
+      if user && user.authenticate(params[:password_digest])
           session[:user_id] = user.id
           render json: user
       else
