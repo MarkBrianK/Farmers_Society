1e533e0e1f70c576d499b258dbc8ebb1ca3d9c0d
MarkBrianK <kariukimarkbrian>
2022-10-16 22:07:03 +0300

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   skip_before_action :authorize, only: :create
 
-  # def index
-  #   users = User.all
-  #   render json: users
-  # end
+  def index
+    users = User.all
+    render json: users
+  end
 
   def show
 
